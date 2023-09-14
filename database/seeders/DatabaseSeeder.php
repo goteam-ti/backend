293135985b9f964c859254170a5643de55b6168e
phojie rengel <phojrengel@gmail.com>
2023-09-14 19:37:30 +0800

    chore(init): config user module

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,21 +4,22 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Admin',
+            'email' => 'phojrengel@gmail.com',
+            'password' => bcrypt('password')
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Generate default seeders
+        $this->call([
+            UserSeeder::class,
+        ]);
     }
 }
