d3cdf482ca28e8d956a159f3f42cc6810dec6f38
phojie rengel <phojrengel@gmail.com>
2023-09-18 05:17:46 +0800

    docs: phpdocs

--- routes/v1/tasks.php
+++ routes/v1/tasks.php
@@ -9,6 +9,10 @@ use App\Http\Controllers\V1\Tasks\StoreController;
 use App\Http\Controllers\V1\Tasks\UpdateController;
 use Illuminate\Support\Facades\Route;
 
+/*
+ * We can actually use the Route::apiResource() method to register a resourceful route to the controller.
+ * However, we will not use it in this one because we want to be explicit in our route declaration.
+ */
 Route::get('/', IndexController::class)->name('index');
 Route::post('/', StoreController::class)->name('store');
 Route::get('/{ulid}', ShowController::class)->name('show');
