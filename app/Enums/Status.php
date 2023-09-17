4a536a08bd58cd0037f3f1ef492a9d9ecd869658
phojie rengel <phojrengel@gmail.com>
2023-09-17 11:57:22 +0800

    docs: init

--- app/Enums/Status.php
+++ app/Enums/Status.php
@@ -6,6 +6,7 @@ namespace App\Enums;
 
 enum Status: string
 {
+    // We can add more statuses here such as 'pending', 'rejected', 'approved', etc.
     case INCOMPLETE = 'incomplete';
     case COMPLETE = 'complete';
 }
