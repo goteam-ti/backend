6c44971bc19ab86027cf1074a8ef929b0d994021
phojie rengel <phojrengel@gmail.com>
2023-09-17 09:43:51 +0800

    chore(deps): fix

--- config/app.php
+++ config/app.php
@@ -163,7 +163,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
         /*
          * Application Service Providers...
