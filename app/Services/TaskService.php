babb1ee0b665b3a90ca46711a688b7ba02c93cfe
phojie rengel <phojrengel@gmail.com>
2023-09-18 12:25:42 +0800

    refactor: improve caching

--- app/Services/TaskService.php
+++ app/Services/TaskService.php
@@ -8,6 +8,7 @@ use App\Models\Task;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
+use Redis;
 
 /**
  * We can use this service to get, create, update, delete, and other task related actions.
@@ -87,9 +88,12 @@ class TaskService
 
     public function clearCache(): void
     {
-        // we can cache using prefix tags and clear all cache with that prefix,
-        // for now we can just do this
-        Cache::tags($this->cacheKey)->flush();
+        // clear all cache with prefix cacheKey
+        $keys = Redis::keys($this->cacheKey . '_*');
+
+        if (!empty($keys)) {
+            Redis::del($keys);
+        }
     }
 
     public function getCacheKey(?Request $request = null): string
